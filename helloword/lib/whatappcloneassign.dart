63ce4149784744f3ceb3fb204918ee1f3699b4ab
Abukar-Hassan-Salah <abukarhassansalah@gmail.com>
2023-11-30 00:08:32 +0300

    whatsapp clone assignment

--- helloword/lib/whatappcloneassign.dart
+++ helloword/lib/whatappcloneassign.dart
@@ -1,13 +1,24 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: 'WhatsApp Clone',
+    home: HomeScreen(),
+  ));
+}
+
 class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(255, 54, 53, 53),
+      backgroundColor: Color.fromARGB(255, 51, 44, 44),
       appBar: AppBar(
-        backgroundColor: Color.fromARGB(255, 51, 44, 44),
-        title: const Text('Chats'),
+        backgroundColor: Color.fromARGB(255, 54, 53, 53),
+        title: const Text(
+          'Chats',
+          style: TextStyle(color: Colors.green),
+        ),
       ),
       body: ListView(
         children: const [
@@ -88,12 +99,81 @@ class StatusScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color.fromARGB(255, 51, 44, 44),
       appBar: AppBar(
         backgroundColor: Color.fromARGB(255, 51, 44, 44),
         title: Text('Status'),
       ),
-      body: Center(
-        child: Text('Status Screen'),
+      body: ListView(
+        children: const [
+          ListTile(
+            textColor: Color.fromARGB(255, 255, 255, 255),
+            leading: CircleAvatar(
+              backgroundImage: AssetImage('assets/bakar.jpg'),
+            ),
+            title: Text('Abukar Hassan'),
+            subtitle: Text('Hello, sidee tahay?'),
+            trailing: Text('1:30 AM'),
+          ),
+          ListTile(
+            textColor: Colors.white,
+            leading: CircleAvatar(
+              backgroundImage: AssetImage('assets/Abukar.jpg'),
+            ),
+            title: Text('Isse Hassan'),
+            subtitle: Text('Haa, fcn alx?'),
+            trailing: Text('1:50 AM'),
+          ),
+          ListTile(
+            textColor: Colors.white,
+            leading: CircleAvatar(
+              backgroundImage: AssetImage('assets/Abukar.jpg'),
+            ),
+            title: Text('Daud Hassan'),
+            subtitle: Text('Hye, Intee joogtaa?'),
+            trailing: Text('11:30 PM'),
+          ),
+          ListTile(
+            textColor: Colors.white,
+            leading: CircleAvatar(
+              backgroundImage: AssetImage('assets/bakar.jpg'),
+            ),
+            title: Text('Hassan Salah'),
+            subtitle: Text('Asc, wilkeyga soma wacnid?'),
+            trailing: Text('5:30 AM'),
+          ),
+          ListTile(
+            textColor: Colors.white,
+            leading: CircleAvatar(
+              backgroundImage: AssetImage('assets/Abukar.jpeg.jpg'),
+            ),
+            title: Text('Abukar Hassan'),
+            subtitle: Text('Fcn, Aabbe ,adna soma wacnid?'),
+            trailing: Text('10:30 AM'),
+          ),
+          ListTile(
+            textColor: Colors.white,
+            subtitleTextStyle:
+                TextStyle(color: Color.fromARGB(255, 209, 118, 118)),
+            leading: CircleAvatar(
+              backgroundImage: AssetImage('assets/Abukar.jpg'),
+            ),
+            title: Text('Daud Hassan'),
+            subtitle: Text('Hye, Intee joogtaa?'),
+            trailing: Text('11:30 PM'),
+          ),
+          ListTile(
+            textColor: Colors.white,
+            leading: CircleAvatar(
+              backgroundImage: AssetImage('assets/bakar.jpg'),
+            ),
+            title: Text('Hassan Salah'),
+            subtitle: Text('Asc, wilkeyga soma wacnid?'),
+            trailing: Text('5:30 AM'),
+          ),
+
+          // Add more chat conversations here
+        ],
       ),
     );
   }
@@ -103,26 +183,118 @@ class CallsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color.fromARGB(255, 105, 104, 104),
       appBar: AppBar(
-        backgroundColor: Color.fromARGB(255, 51, 44, 44),
+        backgroundColor: Color.fromARGB(255, 96, 93, 93),
         title: Text('Calls'),
       ),
-      body: Center(
-        child: Text('Calls Screen'),
+      body: ListView(
+        children: const [
+          ListTile(
+            textColor: Colors.white,
+            leading: CircleAvatar(
+              backgroundImage: AssetImage('assets/bakar.jpg'),
+            ),
+            title: Text('Abukar Hassan'),
+            subtitle: Text('Today, 5:30 AM'),
+            trailing: Icon(
+              Icons.call,
+              color: Colors.green,
+            ),
+          ),
+          ListTile(
+            textColor: Colors.white,
+            leading: CircleAvatar(
+              backgroundImage: AssetImage('assets/Abukar.jpg'),
+            ),
+            title: Text('Isse Hassan'),
+            subtitle: Text('November, 6:30 PM'),
+            trailing: Icon(
+              Icons.call,
+              color: Colors.green,
+            ),
+          ),
+          ListTile(
+            textColor: Colors.white,
+            leading: CircleAvatar(
+              backgroundImage: AssetImage('assets/Abukar.jpg'),
+            ),
+            title: Text('Daud Hassan'),
+            subtitle: Text('Yesterday,7:30 PM'),
+            trailing: Icon(
+              Icons.call,
+              color: Colors.green,
+            ),
+          ),
+          ListTile(
+            textColor: Colors.white,
+            leading: CircleAvatar(
+              backgroundImage: AssetImage('assets/bakar.jpg'),
+            ),
+            title: Text('Hassan Salah'),
+            subtitle: Text('Asc, wilkeyga soma wacnid?'),
+            trailing: Icon(
+              Icons.call,
+              color: Colors.green,
+            ),
+          ),
+          ListTile(
+            textColor: Colors.white,
+            leading: CircleAvatar(
+              backgroundImage: AssetImage('assets/Abukar.jpeg.jpg'),
+            ),
+            title: Text('Abukar Hassan'),
+            subtitle: Text('Fcn, Aabbe ,adna soma wacnid?'),
+            trailing: Icon(
+              Icons.call,
+              color: Colors.green,
+            ),
+          ),
+          ListTile(
+            textColor: Colors.white,
+            subtitleTextStyle:
+                TextStyle(color: Color.fromARGB(255, 209, 118, 118)),
+            leading: CircleAvatar(
+              backgroundImage: AssetImage('assets/Abukar.jpg'),
+            ),
+            title: Text('Daud Hassan'),
+            subtitle: Text('Hye, Intee joogtaa?'),
+            trailing: Icon(
+              Icons.call,
+              color: Colors.green,
+            ),
+          ),
+          ListTile(
+            textColor: Colors.white,
+            leading: CircleAvatar(
+              backgroundImage: AssetImage('assets/bakar.jpg'),
+            ),
+            title: Text('Hassan Salah'),
+            subtitle: Text('Asc, wilkeyga soma wacnid?'),
+            trailing: Icon(
+              Icons.call,
+              color: Colors.green,
+            ),
+          ),
+
+          // Add more chat conversations here
+        ],
       ),
     );
   }
 }
 
 class SettingsScreen extends StatelessWidget {
+  const SettingsScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Color.fromARGB(255, 51, 44, 44),
+        backgroundColor: const Color.fromARGB(255, 51, 44, 44),
         title: Text('Settings'),
       ),
-      body: Center(
+      body: const Center(
         child: Text('Settings Screen'),
       ),
     );
@@ -136,7 +308,7 @@ class HomeScreen extends StatelessWidget {
       length: 4,
       child: Scaffold(
         appBar: AppBar(
-          backgroundColor: Color.fromARGB(255, 51, 44, 44),
+          backgroundColor: const Color.fromARGB(255, 51, 44, 44),
           title: Text('WhatsApp Clone'),
           bottom: const TabBar(
             tabs: [
@@ -158,11 +330,4 @@ class HomeScreen extends StatelessWidget {
       ),
     );
   }
-}
-
-void main() {
-  runApp(MaterialApp(
-    title: 'WhatsApp Clone',
-    home: HomeScreen(),
-  ));
 }
